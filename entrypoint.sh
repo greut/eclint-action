4f05be437d329f071769700c2bbb770d665a0aa3
Yoan Blanc <yoan@dosimple.ch>
2022-07-18 13:14:29 +0200

    feat: allow passing args
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,12 @@ set -xe
 # Fix for Git 2.35.2
 git config --global --add safe.directory "${GITHUB_WORKSPACE:-$(pwd)}"
 
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+  cd "${GITHUB_WORKSPACE}" || exit
+fi
+
 ECLINT=eclint
 
 $ECLINT -version
 
-$ECLINT -color=always
+$ECLINT -color=always ${INPUT_ECLINT_ARGS} ${INPUT_WORKING_DIRECTORY:-"."}
